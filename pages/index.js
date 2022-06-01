919c1fe782c13504eb7615e08b14ef863c92c15f
Sunny Yang <guang.yang@thoughtworks.com>
2022-06-01 11:15:13 +0800

    adjust title

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex items-center width-full flex-col mt-8">
+      <h1 className="text-3xl">
+        Gimme a Direction!
+      </h1>
+    </div>
   )
 }
